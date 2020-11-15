7b9ba86be6a2f45dfa363d040f1d78151aec0811
zhuqishi <1466745070@qq.com>
2020-11-15 21:27:12 +0800

    [CHG]提交显示提交码和显示有效期功能代码

--- commit_code/request_commit_code/models.py
+++ commit_code/request_commit_code/models.py
@@ -3,13 +3,27 @@ from django.db import models
 from django.utils import timezone
 
 class CommitCodeRequest(models.Model):
-    commit_code = models.CharField('提交码', max_length=50)
+    commit_code = models.CharField('提交码', max_length=50, default = "invalid")
     applicant = models.CharField('申请者', max_length=20)
     users = models.CharField('用户', max_length=100)
     code_path = models.CharField('代码路径', max_length=200)
-    start_time = models.DateTimeField('开始时间',default = timezone.now)
-    end_time = models.DateTimeField('结束时间',default = datetime.datetime.now()+datetime.timedelta(weeks=1))
-    b_available = models.BooleanField('在可用期内', default=True)
+    start_time = models.DateTimeField('开始时间', default = timezone.now)
+    end_time = models.DateTimeField('结束时间', default = datetime.datetime.now()+datetime.timedelta(weeks=2))
+    last_request_time = models.DateTimeField('最近申请时间', default = timezone.now)
+
+    def request_commit_code(self):
+        if self.commit_code == "invalid":
+            self.commit_code = "HIK_Test"
+        return self.commit_code
+    request_commit_code.short_description = '提交码'
+
+    def b_available(self):
+        cur_time = timezone.now()
+        if cur_time > self.end_time:
+            return False
+        return cur_time - datetime.timedelta(hours=6) < self.last_request_time
+    b_available.boolean = True
+    b_available.short_description = "在可用期内"
 
     def __str__(self):
         return self.code_path
