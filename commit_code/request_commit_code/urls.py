a39d8ef0fe51cd13dd340321fbc9c992350291c7
zhuqishi <1466745070@qq.com>
2021-04-04 13:04:09 +0800

    [CHG]提交页面修改

--- commit_code/request_commit_code/urls.py
+++ commit_code/request_commit_code/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     # ex: /request_commit_code/
-    path('', views.show_request, name='show_request'),
+    path('', views.show_users_list, name='show_users_list'),
+    path('<user_name>', views.show_user_request_list, name='show_user_request_list'),
+    #path('history/<path:name>/', views.view_history),
     # ex: /request_commit_code/login/
     #path(r'^$', views.show_request, name='show_request'),
 ]
--- commit_code/request_commit_code/views.py
+++ commit_code/request_commit_code/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import CommitCodeRequest
 
-def show_request(request):
-    commitCodes = CommitCodeRequest.objects.all()
-    return render(request, "request_commit_code/showCommitCode.html", {"commitCodes":commitCodes})
+def show_users_list(request):
+    CommitCodeRequests = CommitCodeRequest.objects.all()
+    CommitCodeRequests = CommitCodeRequests.values("users").distinct()
+    return render(request, "request_commit_code/showUsersList.html", {"CommitCodeRequests":CommitCodeRequests})
+
+def show_user_request_list(request,user_name:str):
+    userDataList = CommitCodeRequest.objects.filter(users = user_name)
+    return render(request, "request_commit_code/showUserRequestsList.html", {"userDataList":userDataList})
